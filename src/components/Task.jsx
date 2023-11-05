d74bc0d489036788b9f40fa55d4bd237bd2b36c5
hadijah10 <mohamaduhadijatu@gmail.com>
2023-11-05 21:34:30 +0000

    reusable

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,46 +1,68 @@
-import React, { useState,useEffect } from 'react'
-
-
-import { Box,Stack,styled } from '@mui/material';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-
+import React, { useState, useEffect } from "react";
+import { Box, Stack, styled } from "@mui/material";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function Task() {
-  const Addlist = styled("div")(({})=> ({
-    display:"flex",alignItems:"center",gap:"10px",
-    width :"95%",
-    backgroundColor:"#212121",
-    color:"#bdbdbd",
-    border:"1px solid black",
-  
-}));
-  const [tasks,setTask] = useState(JSON.parse(localStorage.getItem("Tasks")))
-  useEffect(()=> {
-    !tasks?
-      localStorage.setItem("Tasks",JSON.stringify([]))
-    :
-    localStorage.setItem("Tasks",JSON.stringify(tasks))
-  },[tasks])
- const updateTask = (item) => {
-  setTask([...tasks,item])
- }
- 
+  const Addlist = styled("div")(({}) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    width: "95%",
+    backgroundColor: "#212121",
+    color: "#bdbdbd",
+    border: "1px solid black",
+  }));
+  const [tasks, setTask] = useState(
+    JSON.parse(localStorage.getItem("Tasks")) || []
+  );
+  useEffect(() => {
+    tasks
+      ? localStorage.setItem("Tasks", JSON.stringify(tasks))
+      : localStorage.setItem("Tasks", JSON.stringify([]));
+  }, [tasks]);
+  const updateTask = (item) => {
+    // if (!tasks) return;
+    setTask((prev) => [...prev, item]);
+  };
+  const PinTask = (index) => {
+    if (!tasks) return;
+    setTask(
+      tasks.map((data) => {
+        data?.id === index ? { ...tasks, pinned: !data.pinned } : tasks;
+      })
+    );
+  };
+
   return (
-  <>
-    <Stack spaccing={3} sx={{alignItems:"center",minHeight:"60vh",ml:{sm:1,md:3},mr:{sm:3,md:6}}}>
-      <Box sx={{display:"flex",justifyContent:'end',alignContent:"center",minWidth:"100%"}}>
-      <Addlist sx={{pl:1.5,pt:1,pb:1,borderRadius:1}}>
-      <FormatAlignLeftIcon color="#757575" sx={{fontSize:"medium",}}/>
-      <TodoForm updateTask={updateTask}/>
-      </Addlist>
-      </Box>
-      <TodoList tasks= {tasks} setTask={setTask} />
-    </Stack>
-  </>
-  )
+    <>
+      <Stack
+        spaccing={3}
+        sx={{
+          alignItems: "center",
+          minHeight: "60vh",
+          ml: { sm: 1, md: 3 },
+          mr: { sm: 3, md: 6 },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            alignContent: "center",
+            minWidth: "100%",
+          }}
+        >
+          <Addlist sx={{ pl: 1.5, pt: 1, pb: 1, borderRadius: 1 }}>
+            <FormatAlignLeftIcon color="#757575" sx={{ fontSize: "medium" }} />
+            <TodoForm updateTask={updateTask} />
+          </Addlist>
+        </Box>
+        <TodoList tasks={tasks} PinTask={PinTask} />
+      </Stack>
+    </>
+  );
 }
 
 export default Task;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import {v4} from 'uuid';
-import { useForm } from "react-hook-form"
+import React, { useEffect } from "react";
+import { v4 } from "uuid";
+import { useForm } from "react-hook-form";
 
-function TodoForm({updateTask}) {
+function TodoForm({ updateTask }) {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm({
-    defaultValues:{
-      todoitem: "Add a task"
-    }
-  })
-  const listItem = watch('todoitem')
-  const onSubmit= ()=>{
-   let item = {
-      id:v4(),
-      data:listItem,
-      Pinned:false,
-      memo:null
-     }
-   //setTask([...tasks,item])
-   updateTask(item)
-  }
+    defaultValues: {
+      todoitem: "Add a task",
+    },
+  });
+  const listItem = watch("todoitem");
+  const onSubmit = () => {
+    let item = { id: v4(), data: listItem, Pinned: false, memo: null };
+    updateTask(item);
+  };
 
   return (
-      <form style={{width:"90%",display:"flex",flex:1}} onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" 
-          {...register("todoitem", { required: 'This is required', maxLength: 20 })}
-          autoFocus placeholder='Add a task'  style={{ color:"#757575",width:"inherit",pl:1,border:"none",backgroundColor:"#212121",outline:"none"}} id="theinput"/>
-          <p>{errors.todoitem?.message}</p>
-          <button style={{display:"none",outline:"none",}}type='submit' >Submit</button>
-      </form>
-  )
+    <form
+      style={{ width: "90%", display: "flex", flex: 1 }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input
+        type="text"
+        {...register("todoitem", {
+          required: "This is required",
+          maxLength: 20,
+        })}
+        autoFocus
+        placeholder="Add a task"
+        style={{
+          color: "#757575",
+          width: "inherit",
+          pl: 1,
+          border: "none",
+          backgroundColor: "#212121",
+          outline: "none",
+        }}
+        id="theinput"
+      />
+      <p>{errors.todoitem?.message}</p>
+      <button style={{ display: "none", outline: "none" }} type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
